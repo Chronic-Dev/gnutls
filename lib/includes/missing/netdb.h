ec16ed4e9eb1d7c20b514c8393610b020dcbf01d
Nikias Bassen <nikias@gmx.li>
2011-09-10 18:12:19 +0200

    Adapted for win32 (mingw) building

---
+++ lib/includes/missing/netdb.h
@@ -0,0 +1,4 @@
+// This is for win32 only so we don't have to modify the source code.
+#ifndef NETDB_H
+#define NETDB_H
+#endif
---
+++ lib/includes/missing/sys/socket.h
@@ -0,0 +1,7 @@
+// This is for win32 only so we don't have to modify the source code.
+#ifndef SOCKET_H
+#define SOCKET_H
+#ifdef WIN32
+#include <winsock2.h>
+#endif
+#endif
